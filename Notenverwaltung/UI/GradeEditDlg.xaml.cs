54b1f4050abddc44c9caa222ec8df9fe11b823de
marlon-schmitz-289 <marlonschmitz289@gmail.com>
2022-09-15 09:08:01 +0200

    Added: GradeEditDlg

---
+++ Notenverwaltung/UI/GradeEditDlg.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Notenverwaltung
+{
+  public partial class GradeEditDlg : Window
+  {
+    public Subject CurrSub { get; set; }
+
+
+    public GradeEditDlg(Subject sub)
+    {
+      InitializeComponent();
+      CurrSub = sub;
+    }
+
+
+    private void SaveSub(object sender, MouseButtonEventArgs e)
+    {
+
+    }
+  }
+}
