3c07b25a0f21bf6bca2cc0ea7e950da91fb8f7cb
marlon-schmitz-289 <marlonschmitz289@gmail.com>
2022-09-15 10:35:49 +0200

    Update: GradeEditDlg

--- Notenverwaltung/UI/GradeEditDlg.xaml.cs
+++ Notenverwaltung/UI/GradeEditDlg.xaml.cs
@@ -5,19 +5,24 @@ namespace Notenverwaltung
 {
   public partial class GradeEditDlg : Window
   {
-    public Subject CurrSub { get; set; }
+    private Subject CurrSub { get; set; }
+    private Subject ChangedSub { get; set; }
 
 
     public GradeEditDlg(Subject sub)
     {
       InitializeComponent();
       CurrSub = sub;
+      ChangedSub = new(sub.Name, true);
     }
 
 
     private void SaveSub(object sender, MouseButtonEventArgs e)
     {
-
+      if (!Subject.Subjects.Contains(CurrSub!))
+        Subject.Subjects.Add(CurrSub);
+      else
+        Subject.Subjects[Subject.Subjects.IndexOf(CurrSub!)] = ChangedSub!;
     }
   }
 }
