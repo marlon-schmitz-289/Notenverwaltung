60fcde524be6465149646a1e0ea62336cb8cfca6
marlon-schmitz-289 <marlonschmitz289@gmail.com>
2022-09-19 11:23:12 +0200

    Update: Everything works

--- Notenverwaltung/UI/Pages/Subs/EditSubs.xaml.cs
+++ Notenverwaltung/UI/Pages/Subs/EditSubs.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Notenverwaltung
 {
@@ -7,6 +9,28 @@ namespace Notenverwaltung
     public EditSubs()
     {
       InitializeComponent();
+    }
+
+
+    private void Page_Loaded(object sender, System.Windows.RoutedEventArgs e)
+    {
+      FillListBox();
+    }
+
+
+    private void LbxSubs_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+      new SubEditDlg(lbxSubs.SelectedItem as Subject) { Owner = Application.Current.MainWindow }.ShowDialog();
+      FillListBox();
+    }
+
+
+    private void FillListBox()
+    {
+      lbxSubs.Items.Clear();
+
+      foreach(var s in Subject.Subjects)
+        lbxSubs.Items.Add(s);
     }
   }
 }
