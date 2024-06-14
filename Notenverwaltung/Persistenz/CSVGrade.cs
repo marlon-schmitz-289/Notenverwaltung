fede41116b543cdf652f7b7f1348fc305cb53a3e
INTRANET\marlon.schmitz <Marlon.Schmitz@autoelectric.com>
2024-06-14 17:34:29 +0200

    Fixed error on startup when grades file doesnt exist

--- Notenverwaltung/Persistenz/CSVGrade.cs
+++ Notenverwaltung/Persistenz/CSVGrade.cs
@@ -23,46 +23,43 @@ namespace Notenverwaltung.Persistenz
 
         public static void ReadAll ()
         {
-            if (File.Exists(PATH_GRADES))
+            if (!File.Exists(PATH_GRADES))
             {
-                Grades = new();
-                List<string> lines = new();
-                StreamReader sr = new(PATH_GRADES);
-
-                while (!sr.EndOfStream)
-                    lines.Add(sr.ReadLine());
-
-                sr.Close();
-
-                foreach (string line in lines)
-                {
-                    string[] parts = line.Split(';');
-
-                    // Possibility 1:
-                    //Grades.Add(new Grade(
-                    //  Int32.Parse(parts[0]),
-                    //  new(parts[1], false),
-                    //  Int32.Parse(parts[2]),
-                    //  (Type)Int32.Parse(parts[3])),
-                    //  DateTime.Parse(parts[4])
-                    //);
-
-                    // Possibility 2:
-                    Grades.Add(new Grade()
-                    {
-                        Id = int.Parse(parts[0]),
-                        Subject = new(parts[1], true),
-                        Rating = int.Parse(parts[2]),
-                        TypeG = (TypeGrade)int.Parse(parts[3]),
-                        Creation = DateTime.Parse(parts[4])
-                    });
-
-                    return;
-                }
+                File.Create(PATH_GRADES).Close();
             }
 
+            Grades = new();
+            List<string> lines = new();
+            StreamReader sr = new(PATH_GRADES);
 
-            File.Create(PATH_GRADES).Close();
+            while (!sr.EndOfStream)
+                lines.Add(sr.ReadLine());
+
+            sr.Close();
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(';');
+
+                // Possibility 1:
+                //Grades.Add(new Grade(
+                //  Int32.Parse(parts[0]),
+                //  new(parts[1], false),
+                //  Int32.Parse(parts[2]),
+                //  (Type)Int32.Parse(parts[3])),
+                //  DateTime.Parse(parts[4])
+                //);
+
+                // Possibility 2:
+                Grades.Add(new Grade()
+                {
+                    Id = int.Parse(parts[0]),
+                    Subject = new(parts[1], true),
+                    Rating = int.Parse(parts[2]),
+                    TypeG = (TypeGrade)int.Parse(parts[3]),
+                    Creation = DateTime.Parse(parts[4])
+                });
+            }
         }
 
 
